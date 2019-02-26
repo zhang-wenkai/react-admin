a9b4a2c187107aa70b90d4947b7de7d4967eba4f
zhangwenkai <69967696@qq.com>
2019-02-26 18:56:19 +0800

    '完成一级路由的搭建'

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React, {Component} from 'react'
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+
+import Login from './pages/login'
+import Admin from './pages/admin'
 
 export default class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <Router>
+        <Switch>
+          <Route path='/login' component={Login}/>
+          <Route path='/' component={Admin}/>
+        </Switch>
+      </Router>
     )
   }
 }
