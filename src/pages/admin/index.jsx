ae589ff8fe741b753e7abe9f5dd3b7f3c922946c
zhangwenkai <69967696@qq.com>
2019-02-27 15:19:08 +0800

    '完成主页面的布局'

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,9 +1,27 @@
 import React, {Component} from 'react'
+import { Row, Col } from 'antd'
+
+import LeftNav from '../../components/left-nav'
+import Header from '../../components/header'
+import Footer from '../../components/footer'
+
+import './index.less'
 
 export default class Admin extends Component {
   render() {
     return (
-      <div>Admin</div>
+      <Row className='admin'>
+        <Col span={4}>
+          <LeftNav/>
+        </Col>
+        <Col span={20}>
+          <Header/>
+          <div className='admin-main'>
+            main
+          </div>
+          <Footer/>
+        </Col>
+      </Row>
     )
   }
 }
